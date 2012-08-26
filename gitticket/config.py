aed0939f3fb6784cc3f01efeea0f835759c8d8c3
Hiroyuki Tanaka <aflc0x@gmail.com>
2012-08-26 16:21:46 +0900

    Add initial configulation methods.
    主にgitの設定周りの各種メソッドを追加

--- gitticket/config.py
+++ gitticket/config.py
@@ -2,15 +2,51 @@
 # -*- coding:utf-8 -*-
 
 import subprocess as sp
+import os
+import sys
 
-def git():
+def cmd_stdout(arglist):
+    return sp.Popen(arglist, stdout=sp.PIPE).communicate()[0].strip()
+
+def conftodict(config):
     d = {}
-    rawstrs = sp.Popen(['git', 'config', '-l'], stdout=sp.PIPE).communicate()[0].split('\n')
-    configstrs = [x.split('=', 1) for x in filter(None, rawstrs)]
-    for kstr, v in configstrs:
+    for kstr, v in config.items():
         keys = kstr.split('.')
         nd = d
         for k in keys[:-1]:
             nd = nd.setdefault(k, {})
         nd[keys[-1]] = v
     return d
+    
+
+def git():
+    rawstrs = cmd_stdout(('git', 'config', '-l')).split('\n')
+    return dict(x.split('=', 1) for x in filter(None, rawstrs))
+
+def is_inside_work_tree():
+    return cmd_stdout(('git', 'rev-parse', '--is-inside-work-tree')) == 'true'
+
+def git_dir():
+    if not is_inside_work_tree():
+        return None
+    return os.path.abspath(cmd_stdout(('git', 'rev-parse', '-q', '--git-dir')))[:-4] # 必ずついてる末尾の.gitを削除
+
+def guess_repo_name():
+    return os.path.basename(git_dir())
+
+def parseconfig():
+    u"""Parse git config key-values and set for issue handling.
+    name: ticket.name 優先、user.nameが次点
+    repo: ticket.repo 優先、guess_repo_nameが次点 
+    gtoken: github/access_token
+    rtoken: redmine/apikey
+    """
+    gconfig = git()
+    config = {}
+    config['name'] = gconfig.get('ticket.name', gconfig.get('user.name', None)) or sys.exit('Please set ticket.name or user.name to git config file')
+    config['repo'] = gconfig.get('ticket.repo', guess_repo_name())
+    config['gtoken'] = gconfig.get('ticket.github.token', None)
+    config['rtoken'] = gconfig.get('ticket.redmine.token', None)
+    config['rurl'] = gconfig.get('ticket.redmine.url', None)
+    # どこかで使う sys.exit("Please set ticket.token to git config file.\nFor github, use 'git ticket github-authorize to get OAuth access_token.\nFor redmine, set web API key.")    
+    return config
