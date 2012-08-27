f766e71a5c424ecb5fc1982f00f950722211edb3
Hiroyuki Tanaka <aflc0x@gmail.com>
2012-08-27 19:01:02 +0900

    Fix: maybe urljoin can't use {} strings.
    urljoinを使ってformatに渡す{}付きの文字列をくっつけていたらなんか失敗したので、os.path.join
    を使うようにした

--- gitticket/github.py
+++ gitticket/github.py
@@ -2,25 +2,25 @@
 # -*- coding:utf-8 -*-
 
 import json
-import urlparse
 import requests
+import os
 
 BASEURL = 'https://api.github.com'
-AUTH = urlparse.urljoin(BASEURL, 'authorizations')
-REPO = urlparse.urljoin(BASEURL, 'repos/{name}/{repo}')
-ASSIGNEES = urlparse.urljoin(REPO, 'assignees')
-ISSUES = urlparse.urljoin(REPO, 'issues')
-ISSUE = urlparse.urljoin(ISSUES, '{issueid}')
-ISSUE_COMMENTS = urlparse.urljoin(ISSUE, 'comments')
-ISSUE_COMMENT = urlparse.urljoin(ISSUE_COMMENTS, '{commentid}')
-ISSUES_EVENT = urlparse.urljoin(ISSUES, 'events')
-ISSUE_EVENT = urlparse.urljoin(ISSUE, 'events')
-LABELS = urlparse.urljoin(REPO, 'labels')
-LABEL = urlparse.urljoin(LABELS, '{label}')
-ISSUE_LABELS = urlparse.urljoin(ISSUE, 'labels')
-ISSUE_LABEL = urlparse.urljoin(ISSUE_LABELS, '{label}')
-MILESTONES = urlparse.urljoin(REPO, 'milestones')
-MILESTONE = urlparse.urljoin(MILESTONES, '{milestoneid}')
+AUTH = os.path.join(BASEURL, 'authorizations')
+REPO = os.path.join(BASEURL, 'repos/{name}/{repo}')
+ASSIGNEES = os.path.join(REPO, 'assignees')
+ISSUES = os.path.join(REPO, 'issues')
+ISSUE = os.path.join(ISSUES, '{issueid}')
+ISSUE_COMMENTS = os.path.join(ISSUE, 'comments')
+ISSUE_COMMENT = os.path.join(ISSUE_COMMENTS, '{commentid}')
+ISSUES_EVENT = os.path.join(ISSUES, 'events')
+ISSUE_EVENT = os.path.join(ISSUE, 'events')
+LABELS = os.path.join(REPO, 'labels')
+LABEL = os.path.join(LABELS, '{label}')
+ISSUE_LABELS = os.path.join(ISSUE, 'labels')
+ISSUE_LABEL = os.path.join(ISSUE_LABELS, '{label}')
+MILESTONES = os.path.join(REPO, 'milestones')
+MILESTONE = os.path.join(MILESTONES, '{milestoneid}')
 
 def authorize(name, pswd):
     r = requests.post(AUTH, data=json.dumps({'scopes':['repo'], 'note':'git-ticket'}), auth=(name, pswd))
