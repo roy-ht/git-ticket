d3796204c50024a2f63736c8d76b413c8a1f5a5e
Hiroyuki Tanaka <aflc0x@gmail.com>
2012-08-28 18:56:55 +0900

    Refactor: Table class
    Table classからチケットに関する記述をTicketクラスに移譲

--- gitticket/display.py
+++ gitticket/display.py
@@ -3,84 +3,52 @@
 
 import operator as opr
 import itertools as itrt
-from gitticket.config import nested_access
 import blessings
+from gitticket import util
 
 class Table(object):
-    def __init__(self):
-        self.colcfg = {}
-        self.lines = []
-        self.reducename = None
+    def __init__(self, tickets):
+        self.colcfg = []
+        self.tickets = tickets
         self.margin = 2
+        self.reducename = None
     
-    def addcolumn(self, name, **kwargs):
-        kwargs['__ord__'] = len(self.colcfg)
-        self.colcfg[name] = kwargs
-
-    def addjson(self, j):
-        u"""jsonを渡すことにより、それぞれのコラムのkeyに対応して
-        抜き出して文字列リストにする
-        """
-        l = []
-        for col in sorted(self.colcfg.values(), key=opr.itemgetter('__ord__')):
-            l.append(unicode(nested_access(j, col['key'], col.get('default', None))))
-        self.lines.append(l)
+    def addcolumn(self, kwargs):
+        self.colcfg.append(kwargs)
 
     def fit(self):
-        for name, col in self.colcfg.items():
-            col['width'] = max(self.stringwidth(x[col['__ord__']]) for x in self.lines)
-            col['width'] = max(col['width'], self.stringwidth(name))
+        for col in self.colcfg:
+            col['width'] = max(util.strwidth(x[col['name']]) for x in self.tickets)
+            col['width'] = max(col['width'], util.strwidth(col['name']))
         if self.reducename:
-            totwidth = sum(x['width'] for x in self.colcfg.values())
+            totwidth = sum(x['width'] for x in self.colcfg)
             deltawidth = totwidth - termwidth()
             if deltawidth > 0:
-                self.colcfg[self.reducename]['width'] = max(self.colcfg[self.reducename]['width'] - deltawidth, self.stringwidth(self.reducename))
+                self.colcfg[self.reducename]['width'] = max(self.colcfg[self.reducename]['width'] - deltawidth, util.strwidth(self.reducename))
         
-    def stringwidth(self, s):
-        return sum(1 if ord(x) < 256 else 2 for x in s)
-
     def output(self):
-        r = u''
-        for name, col in sorted(self.colcfg.items(), key=lambda x: x[1]['__ord__']):
-            gluelen = col['width'] - self.stringwidth(name)
-            r += name + u' ' * (gluelen + self.margin)
-        r += u'\n'
-        for name, col in sorted(self.colcfg.items(), key=lambda x: x[1]['__ord__']):
-            gluelen = col['width'] - self.stringwidth(name)
-            r += u'-' * (len(name) + gluelen) + u' ' * self.margin
-        r += u'\n'
-        for line in self.lines:
-            for col, t in itrt.izip(sorted(self.colcfg.values(), key=opr.itemgetter('__ord__')), line):
-                gluelen = col['width'] - self.stringwidth(t)
-                if gluelen >= 0:
-                    r += t
-                else:
-                    for i in range(1, len(t) + 1):
-                        gluelen = col['width'] - self.stringwidth(t[:-i]) - 2
-                        if gluelen >= 0:
-                            r += t[:-i] + u'..'
-                            break
-                r += u' ' * (gluelen + self.margin)
-            r += u'\n'
-        return r
+        lines = [[], []]
+        for col in self.colcfg:
+            gluelen = col['width'] - util.strwidth(col['name'])
+            lines[0].append(col['name'] + u' ' * gluelen)
+            lines[1].append(u'-' * (len(col['name']) + gluelen))
+        for ticket in self.tickets:
+            lines.append([ticket.tostr(x['name'], x['width']) for x in self.colcfg])
+        return u'\n'.join((u' ' * self.margin).join(x) for x in lines)
 
 
 def termwidth():
     term = blessings.Terminal()
     return term.width
-        
 
-def json(jsonlist, stgs):
-    u"""stgに入っている情報を元にフォーマットして返す
-    stgはリストで、その順番でコラムを作って返す
-    """
-    t = Table()
-    for stg in stgs:
-        t.addcolumn(stg.get('name', stg['key']), key=stg['key'])
-    for j in jsonlist:
-        t.addjson(j)
+
+def ticket(tickets):
+    u"""Ticketオブジェクトのリストを表示する"""
+    t = Table(tickets)
+    for name in ('id', 'title', 'assign', 'c', 'create', 'update'):
+        if all(getattr(x, name, None) is not None for x in tickets):
+            t.addcolumn({'name':name})
     t.reducename = 'title'
     t.fit()
     return t.output()
-    
-    
+            
