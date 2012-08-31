1bc715cc0d37b0e44cb313f0f831397cd46b6f49
Hiroyuki Tanaka <aflc0x@gmail.com>
2012-08-31 23:14:06 +0900

    Add: util.rmcomment and util.regex_extract
    rmcommentは#で始まるコメントの削除
    regex_extractは、グループ指定された正規表現からマッチした文字列を返す。
    マッチしなかった場合はdefault(何も指定しなければNone)を返す

--- gitticket/util.py
+++ gitticket/util.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 
 import subprocess as sp
+import re
 
 def strwidth(s):
     return sum(1 if ord(x) < 256 else 2 for x in s)
@@ -8,3 +9,14 @@ def strwidth(s):
 def cmd_stdout(arglist):
     return sp.Popen(arglist, stdout=sp.PIPE).communicate()[0].strip()
 
+def rmcomment(s):
+    return s.split('#', 1)[0]
+
+def regex_extract(pattern, tgt, default=None):
+    r = re.search(pattern, tgt, re.M | re.S)
+    if not r:
+        return default
+    grps = r.groups()
+    if len(grps) == 1:
+        return grps[0]
+    return grps
