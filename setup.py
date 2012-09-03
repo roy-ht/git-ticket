08e5ca3c2af2a589f6f6ec8ec13ce27448c6ad18
Hiroyuki Tanaka <aflc0x@gmail.com>
2012-09-03 22:09:03 +0900

    Update: setup.py, prepare to regist to the PyPI server.

--- setup.py
+++ setup.py
@@ -1,12 +1,45 @@
 from setuptools import setup
+import sys
+import os
 
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except Exception:
+        return ''
+
+install_requires = ['distribute', 'blessings', 'requests', 'rauth']
+if sys.hexversion < 0x2070000:
+    install_requires += ['argparse']
 setup(name="gitticket",
-      version='0.1',
-      description="git-ticket",
+      version='0.3',
+      description="Git and issue tracking system integration",
+      long_description=read('README.rst'),
       author='Hiroyuki Tanaka',
       author_email='aflc0x@gmail.com',
       url='https://www.github.com/aflc/git-ticket',
       packages=['gitticket'],
+      install_requires=install_requires,
       entry_points=dict(console_scripts=['git-ticket=gitticket:main']),
-      install_requires=['blessings'],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Natural Language :: Japanese',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2 :: Only'
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+#        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Bug Tracking',
+        'Topic :: Software Development :: Version Control',
+        ],
       zip_safe=False)
+
